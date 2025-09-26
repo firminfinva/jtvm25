5c55e0d2b634c2f4c34095613c40587e22f2b014
firmin nagnduli <finganduli@gmail.com>
2025-09-27 00:35:55 +0200

    fix render error

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\jtvm\kule-ku-mwisho-showcase\app\page.tsx
+// File: D:\jtvm\jtvm\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
